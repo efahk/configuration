9febf536c69713a8850810baf1d34bb9c64d69d5
Kwame Efah <efahakwame@gmail.com>
2024-05-25 19:33:16 -0700

    Updating wezterm config and neovim

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,30 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
+config.default_prog = { "C:\\Program Files\\WindowsApps\\Microsoft.PowerShell_7.4.2.0_x64__8wekyb3d8bbwe\\pwsh" }
 
-config.color_scheme = "Batman"
+-- Color schemes I like :
+-- arcoiris
+config.color_scheme = "Atelierseaside (dark) (terminal.sexy)"
+-- Horizon Dark (Gogh)
+-- rose-pine-moon
+config.enable_tab_bar = false
+-- config.window_decorations = "NONE"
+
+config.window_padding = {
+	left = 25,
+	right = 2,
+	top = 10,
+	bottom = 10,
+}
+
+config.font = wezterm.font_with_fallback({
+	"Hack Nerd Font Mono",
+})
+
+config.window_background_image = "C:\\Users\\kefah\\.config\\wezterm\\backgrounds\\monsterav2.jpg"
+config.window_background_image_hsb = {
+	brightness = 0.1,
+}
 
 return config
